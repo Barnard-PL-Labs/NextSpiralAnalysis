185e4f0aefa5e7efcdcbfdd25138098a35ccf3fb
runzhe lan <lanrunzhe@gmail.com>
2025-05-08 18:41:31 +0800

    correct the font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 //Most of this page is in its default condition except the last few lines 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { EB_Garamond, Cinzel } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/lib/authProvider";
 
@@ -12,6 +13,20 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const ebGaramond = EB_Garamond({
+  variable: "--font-eb-garamond",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+});
+
+const cinzel = Cinzel({
+  variable: "--font-cinzel",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
